0ea76c947f2912be54c856ef41cf8dc5911d1947
as6325400 <as6325400@gmail.com>
2024-06-10 19:42:36 +0800

    rebuild Clan class

--- model/clan.py
+++ model/clan.py
@@ -9,16 +9,16 @@ load_dotenv()
 class Clan():
     def __init__(self, tag):
         self.base_request_url = f"{os.getenv('REQUEST_URL')}clans/"
-        self.tag = tag
-    
-    def clan_exist(self):
-        url = f"{self.base_request_url}%23{self.tag[1:]}"
-        headers = {
+        self.tag = "%23" + tag[1:]
+        self.headers = {
             "Accept": "application/json",
             "authorization": f"Bearer {os.getenv('CLASHOFCLAN_TOKEN')}"
         }
+    
+    def clan_exist(self):
+        url = f"{self.base_request_url}{self.tag}"
         try:
-            response = requests.get(url, headers=headers)
+            response = requests.get(url, headers=self.headers)
             response.raise_for_status()
             if(response.status_code == 200):
                 return True
