586d6238960a6c8c50f11d0c89ce4f8cc004dfb9
as6325400 <as6325400@gmail.com>
2024-06-11 00:23:21 +0800

    add init app

--- app.py
+++ app.py
@@ -1,4 +1,86 @@
-from model.clan import Clan
+import os
+import sys
+from argparse import ArgumentParser
 
-clan = Clan("#RP0CQLQR")
-clan.clan_exist()
+from flask import Flask, request, abort
+from linebot.v3 import (
+     WebhookHandler
+)
+from linebot.v3.exceptions import (
+    InvalidSignatureError
+)
+from linebot.v3.webhooks import (
+    MessageEvent,
+    TextMessageContent,
+)
+from linebot.v3.messaging import (
+    Configuration,
+    ApiClient,
+    MessagingApi,
+    ReplyMessageRequest,
+    TextMessage
+)
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+app = Flask(__name__)
+
+# get channel_secret and channel_access_token from your environment variable
+channel_secret = os.getenv('CHANNEL_SECRET', None)
+channel_access_token = os.getenv('CHANNEL_ACCESS_TOKEN', None)
+port = os.getenv('PORT', 5000)
+if channel_secret is None:
+    print('Specify LINE_CHANNEL_SECRET as environment variable.')
+    sys.exit(1)
+if channel_access_token is None:
+    print('Specify LINE_CHANNEL_ACCESS_TOKEN as environment variable.')
+    sys.exit(1)
+
+handler = WebhookHandler(channel_secret)
+
+configuration = Configuration(
+    access_token=channel_access_token
+)
+
+
+@app.route("/callback", methods=['POST'])
+def callback():
+    print("callback")
+    # get X-Line-Signature header value
+    signature = request.headers['X-Line-Signature']
+
+    # get request body as text
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
+
+    # handle webhook body
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        abort(400)
+
+    return 'OK'
+
+
+@handler.add(MessageEvent, message=TextMessageContent)
+def message_text(event):
+    with ApiClient(configuration) as api_client:
+        line_bot_api = MessagingApi(api_client)
+        line_bot_api.reply_message_with_http_info(
+            ReplyMessageRequest(
+                reply_token=event.reply_token,
+                messages=[TextMessage(text=event.message.text)]
+            )
+        )
+
+
+if __name__ == "__main__":
+    arg_parser = ArgumentParser(
+        usage='Usage: python ' + __file__ + ' [--port <port>] [--help]'
+    )
+    arg_parser.add_argument('-p', '--port', default=5000, help='port')
+    arg_parser.add_argument('-d', '--debug', default=False, help='debug')
+    options = arg_parser.parse_args()
+    app.run(debug=options.debug, port=options.port)
