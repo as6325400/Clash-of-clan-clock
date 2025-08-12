0beb39f44c24e2188dfa644b738f67599278f6ed
Ted-Hsu0630 <w42622@gmail.com>
2025-08-12 23:57:52 +0800

    modified:   app.py

--- app.py
+++ app.py
@@ -244,7 +244,7 @@ def handle_message(event: PostbackEvent):
             elif data["state"] == "inWar":
                 reply_text += f"部落戰將於台北時間 {data['end_time']['hours_taipei']}:{data['end_time']['minutes_taipei']} 結束\n"
                 reply_text += f"剩餘 {data['end_time']['hours_remaining']} 小時 {data['end_time']['minutes_remaining']} 分 \n\n"
-                reply_text += f"目前星數 : {data['ours']['stars']} - {data['theirs']['stars']}（總星數：{data['max_stars']}）\n\n"
+                reply_text += f"目前星數：{data['ours']['stars']} - {data['theirs']['stars']}（總星數：{data['max_stars']}）\n\n"
                 reply_text += "尚未進攻的成員：\n"
             
             elif data["state"] == "preparation":
@@ -275,7 +275,7 @@ def handle_message(event: PostbackEvent):
             elif data["state"] == "inWar":
                 reply_text += f"聯賽進行中\n"
                 reply_text += f"剩餘 {data['end_time']['hours_remaining']} 小時 {data['end_time']['minutes_remaining']} 分\n\n"
-                reply_text += f"目前星數 : {data['ours']['stars']} - {data['theirs']['stars']}\n\n"
+                reply_text += f"目前星數：{data['ours']['stars']} - {data['theirs']['stars']}\n\n"
                 if len(data["member_list"]) == 0:
                     reply_text += "全員皆完成部落聯賽\n"
                 else:
