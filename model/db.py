1622e3e2092cf0983f54814a3e54aa16442b8fb7
Ted-Hsu0630 <w42622@gmail.com>
2025-08-12 16:33:39 +0800

    Update db.py

--- model/db.py
+++ model/db.py
@@ -19,31 +19,31 @@ class DB():
             "state": "fail",
             "message": ""
         }
-        self.cursor.execute(f"Select * from linegroup where group_id = '{group_id}'")
+        self.cursor.execute("Select * from linegroup where group_id = %s", (group_id,))
         group = self.cursor.fetchone()
         print("群組測試", group)
         if group != None:
             inform["message"] = "此群組已經設定過了"
             return inform
         
-        self.cursor.execute(f"Insert into linegroup (group_id) values ('{group_id}')")
+        self.cursor.execute("Insert into linegroup (group_id) values (%s)", (group_id,))
         self.conn.commit()
         group_table_id = self.cursor.lastrowid
         
-        self.cursor.execute(f"Select * from clan where tag = '{clan_tag}'")
+        self.cursor.execute("Select * from clan where tag = %s", (clan_tag,))
         
         clan = self.cursor.fetchone()
         clan_table_id = 0
         
         if clan == None:
-            self.cursor.execute(f"Insert into clan (tag, name) values ('{clan_tag}', '{clan_name}')")
+            self.cursor.execute("Insert into clan (tag, name) values (%s, %s)", (clan_tag, clan_name))
             self.conn.commit()
             clan_table_id = self.cursor.lastrowid
         else:
-            self.cursor.execute(f"Select id from clan where tag = '{clan_tag}'")
+            self.cursor.execute("Select id from clan where tag = %s", (clan_tag,))
             clan_table_id = self.cursor.fetchone()[0]
         
-        self.cursor.execute(f"Insert into clan_group_relationship (group_id, clan_id) values ('{group_table_id}', '{clan_table_id}')")
+        self.cursor.execute("Insert into clan_group_relationship (group_id, clan_id) values (%s, %s)", (group_table_id, clan_table_id))
         self.conn.commit()
         inform["state"] = "success"
         inform["message"] = f"部落 {clan_name} 設定成功"    
@@ -51,16 +51,16 @@ class DB():
     
     def get_clan_by_group_id(self, group_id):
         print("group_id", group_id)
-        self.cursor.execute(f"Select * from linegroup where group_id = '{group_id}'")
+        self.cursor.execute("Select * from linegroup where group_id = %s", (group_id,))
         res = self.cursor.fetchone()
         if res == None:
             return None
         gid = res[0]
-        self.cursor.execute(f"Select clan_id from clan_group_relationship where group_id = '{gid}'")
+        self.cursor.execute("Select clan_id from clan_group_relationship where group_id = %s", (gid,))
         clan_id = self.cursor.fetchone()
         if clan_id == None:
             return None
-        self.cursor.execute(f"Select * from clan where id = '{clan_id[0]}'")
+        self.cursor.execute("Select * from clan where id = %s", (clan_id[0],))
         clan = self.cursor.fetchone()
         return clan[1]
     
@@ -69,22 +69,22 @@ class DB():
             "state": "fail",
             "message": ""
         }
-        self.cursor.execute(f"Select * from linegroup where group_id = '{group_id}'")
+        self.cursor.execute("Select * from linegroup where group_id = %s", (group_id,))
         group = self.cursor.fetchone()
         if group == None:
             inform["message"] = "此群組尚未設定過"
             return inform
         
         gid = group[0]
-        self.cursor.execute(f"Select * from clan_group_relationship where group_id = '{gid}'")
+        self.cursor.execute("Select * from clan_group_relationship where group_id = %s", (gid,))
         relationship = self.cursor.fetchone()
         if relationship == None:
             inform["message"] = "此群組尚未設定過"
             return inform
         
-        self.cursor.execute(f"Delete from clan_group_relationship where group_id = '{gid}'")
+        self.cursor.execute("Delete from clan_group_relationship where group_id = %s", (gid,))
         self.conn.commit()
-        self.cursor.execute(f"Delete from linegroup where group_id = '{group_id}'")
+        self.cursor.execute("Delete from linegroup where group_id = %s", (group_id,))
         self.conn.commit()
         inform["state"] = "success"
         inform["message"] = "刪除成功"
