693a27beef2aa8b4a16a7072d1a3d650fc15725f
as6325400 <as6325400@gmail.com>
2025-08-14 11:25:25 +0800

    update flex

--- src/flex.py
+++ src/flex.py
@@ -1,79 +1,89 @@
 flex_message = {
     "type": "flex",
-    "altText": "clock",  # alt_text
+    "altText": "clock",
     "contents": {
         "type": "bubble",
         "footer": {
             "type": "box",
             "layout": "vertical",
-            "spacing": "sm",
+            "paddingAll": "0px",
+            "spacing": "none",
             "contents": [
                 {
-                    "type": "button",
-                    "style": "link",
-                    "height": "sm",
-                    "action": {
-                        "type": "postback",
-                        "label": "部落聯賽",
-                        "data": "action=Cwl"
-                    }
-                },
-                {
-                    "type": "button",
-                    "style": "link",
-                    "height": "sm",
-                    "action": {
-                        "type": "postback",
-                        "label": "部落戰",
-                        "data": "action=War"
-                    }
-                },
-                {
-                    "type": "button",
-                    "style": "link",
-                    "height": "sm",
-                    "action": {
-                        "type": "postback",
-                        "label": "突襲進攻資訊",
-                        "data": "action=Capital_Info"
-                    }
-                },
-                {
-                    "type": "button",
-                    "style": "link",
-                    "height": "sm",
-                    "action": {
-                        "type": "postback",
-                        "label": "突襲尚未參與",
-                        "data": "action=Capital_not_start"
-                    }
+                    "type": "box",
+                    "layout": "horizontal",
+                    "spacing": "none",
+                    "contents": [
+                        {
+                            "type": "image",
+                            "url": "https://raw.githubusercontent.com/as6325400/Clash-of-clan-clock/refs/heads/main/src/img/image_part_001.jpg",
+                            "size": "full",
+                            "aspectMode": "cover",
+                            "aspectRatio": "1:1",
+                            "flex": 1,
+                            "action": {"type": "postback", "data": "action=Cwl"},
+                        },
+                        {
+                            "type": "image",
+                            "url": "https://raw.githubusercontent.com/as6325400/Clash-of-clan-clock/refs/heads/main/src/img/image_part_002.jpg",
+                            "size": "full",
+                            "aspectMode": "cover",
+                            "aspectRatio": "1:1",
+                            "flex": 1,
+                            "action": {
+                                "type": "postback",
+                                "data": "action=Capital_Info",
+                            },
+                        },
+                        {
+                            "type": "image",
+                            "url": "https://raw.githubusercontent.com/as6325400/Clash-of-clan-clock/refs/heads/main/src/img/image_part_003.jpg",
+                            "size": "full",
+                            "aspectMode": "cover",
+                            "aspectRatio": "1:1",
+                            "flex": 1,
+                            "action": {"type": "postback", "data": "action=Setting"},
+                        },
+                    ],
                 },
                 {
                     "type": "box",
-                    "layout": "vertical",
-                    "contents": [],
-                    "margin": "sm"
+                    "layout": "horizontal",
+                    "spacing": "none",
+                    "contents": [
+                        {
+                            "type": "image",
+                            "url": "https://raw.githubusercontent.com/as6325400/Clash-of-clan-clock/refs/heads/main/src/img/image_part_004.jpg",
+                            "size": "full",
+                            "aspectMode": "cover",
+                            "aspectRatio": "1:1",
+                            "flex": 1,
+                            "action": {"type": "postback", "data": "action=War"},
+                        },
+                        {
+                            "type": "image",
+                            "url": "https://raw.githubusercontent.com/as6325400/Clash-of-clan-clock/refs/heads/main/src/img/image_part_005.jpg",
+                            "size": "full",
+                            "aspectMode": "cover",
+                            "aspectRatio": "1:1",
+                            "flex": 1,
+                            "action": {
+                                "type": "postback",
+                                "data": "action=Capital_not_start",
+                            },
+                        },
+                        {
+                            "type": "image",
+                            "url": "https://raw.githubusercontent.com/as6325400/Clash-of-clan-clock/refs/heads/main/src/img/image_part_006.jpg",
+                            "size": "full",
+                            "aspectMode": "cover",
+                            "aspectRatio": "1:1",
+                            "flex": 1,
+                            "action": {"type": "postback", "data": "action=Introduce"},
+                        },
+                    ],
                 },
-                {
-                    "type": "button",
-                    "action": {
-                        "type": "postback",
-                        "label": "設定教學",
-                        "data": "action=Setting"
-                    },
-                    "height": "sm"
-                },
-                {
-                    "type": "button",
-                    "action": {
-                        "type": "postback",
-                        "label": "介紹教學",
-                        "data": "action=Introduce"
-                    },
-                    "height": "sm"
-                }
             ],
-            "flex": 0
-        }
-    }
+        },
+    },
 }
